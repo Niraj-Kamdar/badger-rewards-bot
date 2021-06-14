c48b038d655b9607ae11b9055a1c656feb36d172
Niraj Kamdar <nirajkamdar27@pm.me>
2021-06-14 19:59:05 +0530

    refactor: sort import and reformat

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 import json
 import os
+from decimal import Decimal, getcontext
 from statistics import mean, median
 
 import boto3
+
+getcontext().prec = 28
 
 
 def download_tree(fileName, test=False):
@@ -58,25 +61,27 @@ def formatter(merkle_data):
 
 def summary(rewards_tree):
     token_dist_data = {
-        key: list(map(lambda x: x["totals"]["0x3472A5A71965499acd81997a54BBA8D852C6E53d"], val.values()))
-        for key, val in rewards_tree["userData"].items() if "digg" not in key.lower()
+        key: list(
+            map(
+                lambda x: x["totals"]["0x3472A5A71965499acd81997a54BBA8D852C6E53d"],
+                val.values(),
+            )
+        )
+        for key, val in rewards_tree["userData"].items()
+        if "digg" not in key.lower()
     }
-    
-    return {
-        key: _list_summary(val)
-        for key, val in token_dist_data.items()
-    }
-    
+
+    return {key: _list_summary(val) for key, val in token_dist_data.items()}
+
+
 def _list_summary(array):
-    from decimal import Decimal, getcontext
-    getcontext().prec = 28
     count = len(array)
-    array = list(map(lambda x: Decimal(x)/ Decimal(10**18), array))
+    array = list(map(lambda x: Decimal(x) / Decimal(10 ** 18), array))
     return dict(
         max=str(max(array)),
         min=str(min(array)),
         mean=str(mean(array)),
         sum=str(sum(array)),
         median=str(median(array)),
-        count=count
+        count=count,
     )

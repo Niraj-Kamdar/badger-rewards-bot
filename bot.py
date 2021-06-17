0e59d230990d6295f3e33beaa73a9ae1f217657a
Niraj Kamdar <nirajkamdar27@pm.me>
2021-06-17 14:13:51 +0530

    refactor: format the code

--- bot.py
+++ bot.py
@@ -57,8 +57,8 @@ def _parse_merkle_data(
 
 @bot.command(name="rewards")
 async def rewards(ctx):
-    formatted_data = (cache["formatted_data"])
-    await ctx.send(embed = formatted_data)
+    formatted_data = cache["formatted_data"]
+    await ctx.send(embed=formatted_data)
 
 
 @bot.event
@@ -70,8 +70,8 @@ async def on_ready():
 async def update_rewards():
     for event in event_filter.get_new_entries():
         _parse_merkle_data(*event["args"])
-        formatted_data = (cache["formatted_data"])
-        await channel.send(embed = formatted_data)
+        formatted_data = cache["formatted_data"]
+        await channel.send(embed=formatted_data)
 
 
 def start():
